1e69ee2f309c62bbed3ecf9e06ec62e82548f83c
YovelNehmady <yovelnehmady1998@gmail.com>
2023-01-10 17:17:08 +0200

    margin the backend

---
+++ mister-toy/backend/services/util.service.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+module.exports = {
+    makeId,
+    writeToysToFile
+}
+
+function writeToysToFile(bugs) {
+    return new Promise((res, rej) => {
+        const data = JSON.stringify(bugs, null, 2)
+        fs.writeFile('data/toy.json', data, (err) => {
+            if (err) return rej(err)
+            res()
+        })
+    })
+}
+
+
+function makeId(length = 5) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
